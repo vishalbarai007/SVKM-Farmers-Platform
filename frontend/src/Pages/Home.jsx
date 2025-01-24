aec445846f55d6ca5b72f7382f5803775f1b54ba
ashish@358 <ashishdhadekar358@gmail.com>
2025-01-24 11:35:43 +0530

    ..

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Navbar from '../Components/Developer/Header';
 import { Slider } from '../Components/Developer/Slider';
+import Services from '../Components/Developer/Services';
+import { CardHoverEffectDemo } from '../Components/Aceternity/Main/Card';
 
 const Home = () => {
   return (
-    <div className='bg-zinc-950 text-white h-screen flex justify-center items-center'>
-      {/* hello  */}
-      {/* <Navbar/> */}
+    <div className='bg-zinc-950 text-white h-screen flex justify-center items-center flex-col'>
       <Slider/>
+      {/* <Services/> */}
+      <CardHoverEffectDemo/>
     </div>
   )
 }
