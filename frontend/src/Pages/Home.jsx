3a910f4a4ea49a8a8541c3fdcd45836d54745ce6
Raj Vishwakarma <rajrv6929@gmail.com>
2025-01-24 15:30:38 +0530

    changes

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -5,15 +5,22 @@ import Header from '../Components/Developer/Header'
 import { CardHoverEffectDemo } from '../Components/Aceternity/Main/Card';
 import { AppleCardsCarouselDemo } from '../Components/Aceternity/Main/Applecard';
 import ChatBot from '../Components/Developer/ChatBot';
+import { useLocation } from 'react-router-dom';
+
 // import Dashboard from '../Components/Developer/DashboardOptions';
 // import Navbar from '../Components/Developer/Header';
 // import { Carousel } from '../Components/Aceternity/Support/carousel';
 // import { CarouselDemo } from '../Components/Aceternity/Main/HomeCarousel';
 
 const Home = () => {
+  
+  const location = useLocation();
+  console.log("state: ", location.state);
+  const uid = location.state?.uid || null;
+  console.log(uid);
   return (
     <div className=' text-white h-screen '>
-      <Header/>
+      <Header uid={uid}/>
       {/* <Slider/> */}
       {/* <Carousel/> */}
       {/* <LandingHome/> */}
